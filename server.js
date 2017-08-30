73d20e5502276061896b7d5213a676b1fafb0653
Harjeet Singh <harjeet.singh@fireeye.com>
2017-08-30 12:07:20 -0700

    added static content server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
 
 const server = new Hapi.Server();
 server.connection({
@@ -42,6 +43,30 @@ server.register(require('inert'), (err) => {
       reply.file('./public/hello.html');
     }
   });
+});
+
+
+server.register({
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
+    }
+  }
+}, (err) => {
+
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
 });
 
 server.start((err) => {
